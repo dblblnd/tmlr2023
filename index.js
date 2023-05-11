ae47d3ad30139b99d26dc68e34e598ded1c0fa4d
dblblnd <acl2019anon@gmail.com>
2023-05-10 23:20:00 -0400

    Adds tabs

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-(function ($) {
+(function () {
 
   const SAMPLES_ROOT = 'https://dblblnd.github.io/neurips23/samples';
 
   async function onDomReady() {
+    // Initialize synchronized MIDI / audio playback visualizer
     const audioMidiPlayerTemplate = document.querySelector('#audio-midi-player-template');
     document.querySelectorAll('div.audio-midi-player').forEach((player, i) => {
-      const midiSample = player.getAttribute('midi-sample');
-      const mp3Sample = player.getAttribute('mp3-sample');
+      const exampleTag = player.getAttribute('example');
 
       // Grab elements
       const content = audioMidiPlayerTemplate.content.cloneNode(true);
@@ -14,17 +14,16 @@
       const midiPlayer = content.querySelector('midi-player');
       const audioPlayer = content.querySelector('audio');
       const audioSource = content.querySelector('source');
-      console.log(midiPlayer);
 
       // Configure MIDI player
       midiVisualizer.setAttribute('id', `audio-midi-player-${i}`);
       midiPlayer.setAttribute('visualizer', `#audio-midi-player-${i}`);
-      const midiSrc = `${SAMPLES_ROOT}/${midiSample}`;
+      const midiSrc = `${SAMPLES_ROOT}/${exampleTag}.vis.mid`;
       midiVisualizer.setAttribute('src', midiSrc);
       midiPlayer.setAttribute('src', midiSrc);
 
       // Configure audio player
-      const audioSrc = `${SAMPLES_ROOT}/${mp3Sample}`;
+      const audioSrc = `${SAMPLES_ROOT}/${exampleTag}.mp3`;
       audioSource.setAttribute('src', audioSrc);
 
       // Bind audio controls to MIDI player controls
@@ -44,29 +43,34 @@
         audioPlayer.play();
       });
       midiPlayer.addEventListener('stop', function () {
-        console.log('stop', midiPlayer.currentTime);
         audioPlayer.pause();
       });
 
       player.appendChild(content);
     });
 
-    /*
-    $("#inpainted-music a, #controll-number-of-bars a").click(function () {
-      // switch to the selected model
-      const songNumber = $(this).parent().parent().attr('id')
-      const midiUrl = $(this).attr('midi-url')
-      const midiPlayerId = songNumber + '-' + 'player'
-      const midiVisualizerId = songNumber + '-' + 'visualizer'
-      $('#' + midiPlayerId).attr("src", midiUrl);
-      $('#' + midiVisualizerId).attr("src", midiUrl);
-
-      // set the anchor tag selected
-      $('#' + songNumber + ' a.selected').removeAttr('class')
-      $(this).attr('class', 'selected')
+    // Initialize tab switchers
+    document.querySelectorAll('div.example-tabbed').forEach((div, i) => {
+      const midiVisualizer = div.querySelector('midi-visualizer');
+      const midiPlayer = div.querySelector('midi-player');
+      const audioPlayer = div.querySelector('audio');
+      const audioSource = div.querySelector('source');
+      div.querySelectorAll('a').forEach((a, j) => {
+        const exampleTag = a.getAttribute('example');
+        a.onclick = function () {
+          div.querySelectorAll('a').forEach((_a) => { _a.className = ''; });
+          a.className = 'selected';
+          const midiSrc = `${SAMPLES_ROOT}/${exampleTag}.vis.mid`;
+          const audioSrc = `${SAMPLES_ROOT}/${exampleTag}.mp3`;
+          midiVisualizer.setAttribute('src', midiSrc);
+          midiPlayer.setAttribute('src', midiSrc);
+          audioPlayer.pause();
+          audioSource.setAttribute('src', audioSrc);
+          audioPlayer.load();
+        };
+      });
     });
-    */
   }
 
   document.addEventListener("DOMContentLoaded", onDomReady, false);
-})(window.$);
+})();
