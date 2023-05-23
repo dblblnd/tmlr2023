bc6745ff466024c1b64378033891b79ba8f69a81
dblblnd <acl2019anon@gmail.com>
2023-05-23 19:53:56 -0400

    Increased page width

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
   const SAMPLES_ROOT = 'https://dblblnd.github.io/neurips23/samples';
   // Controls horizontal width (default 30)
-  const PIXELS_PER_TIME_STEP = 38;
+  const PIXELS_PER_TIME_STEP = 49;
   // Controls vertical width
   const NOTE_HEIGHT = 3;
 
